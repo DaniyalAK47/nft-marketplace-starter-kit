eca8e2e1d3a9909ab550639893f1b99d82cd45f8
Daniyal Khan <daniyal@block360.io>
2022-12-09 17:32:57 +0500

    truffle setup

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+contract Kryptobirds{
+
+    string public name;
+    string public symbol;
+
+    constructor(){
+        name = 'Kryptobird';
+        symbol = 'KBIRDZ';
+    }
+}
